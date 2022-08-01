13f845043ed423e2a666e24224c3c318c3796ff5
Tiri <shaymusts@gmail.com>
2022-08-01 12:13:48 +0200

    prisma migrations and schema files

---
+++ prisma/migrations/20220731164902_removed_fav_color_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `favColor` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "favColor";
