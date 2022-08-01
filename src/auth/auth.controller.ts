d51501b848e9841549b3178008bf898913a2bbb0
Tiri <shaymusts@gmail.com>
2022-08-01 12:16:27 +0200

    sign in and sign up logic completed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/userCredentials.dto';
 
-@Controller('auth')
-export class AuthController {}
+@Controller("auth")
+export class AuthController {
+  constructor(private readonly authService: AuthService){}
+
+  @Post("signup")
+  signUp(@Body() authCredentials: AuthDto): Promise<User> {
+    return this.authService.signUp(authCredentials)
+  }
+
+  @Post("login")
+  login(@Body() authCredentials: AuthDto) {
+    return this.authService.login(authCredentials)
+  }
+}
